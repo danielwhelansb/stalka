9c44431f63639f08ca56b2de50407a74e5627a67
danw <daniel.whelan@sensis.com.au>
2012-08-02 15:22:09 +1000

    Added tests

--- README.md
+++ README.md
@@ -1,34 +1,10 @@
 Stalka
 ======
 
-## Simple - Defaults to reading/writing sequence from/to <baseDbUri>/_local/feed (lastSequence)
-	var stalka = require('./stalka')(changeWriter);
+## Getting Started
+	var stalka = require('stalka');
 	function changeWriter(changes, callback) {
 	  console.log("Writing changes...");
 	  callback();
 	}
-
-## Custom Sequence Management
-	var stalka = require('./stalka')(changeWriter, {
-	  sequenceWriter: sequenceWriter,
-	  sequenceReader: sequenceReader
-	});
-
-	function sequenceReader(baseDbUri, callback) {
-	  console.log("Reading sequence from ", baseDbUri);
-	  callback(1);
-	}
-
-	function sequenceWriter(baseDbUri, sequence, callback) {
-	  console.log("Writing sequence %s to %s", sequence, baseDbUri);
-	  callback();
-	}
-
-## Simple - Starts at either at 0 or from the _local/feed -> lastSequence, Limit defaults to 10
-	stalka.start('http://localhost:5984/about');
-
-## Specific Start Sequence - Starts at 25, Limit: defaults to 10
-	stalka.start('http://localhost:5984/about', {since: 10});
-
-## Specific Start Sequence and limit, Starts at 25, Limit to 50
-	stalka.start('http://localhost:5984/about', {since: 10, limit: 25});
+	stalka.start("http://somehost:5984/somedb", changeWriter);
---
+++ test/stalka.js
@@ -0,0 +1,349 @@
+var should = require("should");
+var sandbox = require('sandboxed-module');
+
+var libpath = process.env['STALKA_COV'] ? '../lib-cov' : '../lib';
+
+// jscoverage support. This allows the global to be copied across to the new sandboxed context.
+if (typeof _$jscoverage === 'undefined') _$jscoverage = {};
+
+var fakenano = function(dbUri) {
+  return  {
+    get: function(id, callback) {
+    },
+    insert: function(doc, id, callback) {
+    }
+  };
+};
+
+var fakerequest = function(url, callback) {};
+
+var stalka = sandbox.require(libpath + '/stalka', {
+  requires: {'nano': fakenano, 'request': fakerequest}
+});
+
+describe('Stalka', function() {
+  describe('#readSequence', function() {
+    it("should use _local/feed as the sequence document id", function(done) {
+      var db = { 
+        get: function(id, callback) {
+          id.should.equal("_local/feed");
+          callback();
+        }
+      };
+      stalka.readSequence(db, function(err, body) {
+        done();
+      });
+    }),
+    it("should return sequence document when its found", function(done) {
+      var db = { 
+        get: function(id, callback) {
+          id.should.equal("_local/feed");
+          callback(null, {lastSequence: 13}); 
+        }
+      };
+      stalka.readSequence(db, function(err, body) {
+        should.deepEqual(body, {lastSequence: 13});
+        done();
+      });
+    }),
+    it("should return a lastSequence of 0 when its not found", function(done) {
+      var db = { 
+        get: function(id, callback) { callback({status_code: 404}, null); }
+      };
+
+      stalka.readSequence(db, function(err, body) {
+        should.deepEqual(body, {lastSequence: 0});
+        done();
+      });
+    }),
+    it("should return an error when an error occurs", function(done) {
+      var db = { 
+        get: function(id, callback) { callback("Failed to read sequence.", null); }
+      };
+
+      stalka.readSequence(db, function(err, body) {
+        should.not.exist(body);
+        err.should.equal("Failed to read sequence.");
+        done();
+      });
+    })
+  }),
+  describe('#updateSequence', function() {
+    it("should use _local/feed as the sequence document id", function(done) {
+      var db = { 
+        insert: function(document, id, callback) {
+          id.should.equal("_local/feed");
+          callback();
+        }
+      };
+      stalka.updateSequence(db, {}, function(err, body) {
+        done();
+      });
+    }),
+    it("should insert the given sequence document", function(done) {
+      var db = { 
+        insert: function(document, id, callback) {
+          document.should.eql({something: 123});
+          callback();
+        }
+      };
+      stalka.updateSequence(db, {something: 123}, function(err, body) {
+        done();
+      });
+    }),
+    it("should return the result from a successful insert", function(done) {
+      var db = { 
+        insert: function(document, id, callback) {
+          callback(null, document, null);
+        }
+      };
+      stalka.updateSequence(db, {something: 123}, function(err, body) {
+        body.should.eql({something: 123});
+        done();
+      });
+    }),
+    it("should return the error from an unsuccessful insert", function(done) {
+      var db = { 
+        insert: function(document, id, callback) {
+          callback("Failed to insert", null, null);
+        }
+      };
+      stalka.updateSequence(db, {something: 123}, function(err, body) {
+        should.not.exist(body);
+        err.should.equal("Failed to insert");
+        done();
+      });
+    });
+  }),
+  describe('#readChanges', function() {
+    it("should default the feed to 'longpoll'", function(done) {
+      var stalka = fakeRequestStalka(function(url, callback) {
+        url.should.include("feed=longpoll");
+        callback();
+      });
+
+      stalka.readChanges("http://somehost:1234/somedb", null, function(err, changes) {
+        done();
+      });
+    }),
+    it("should query with any extra options", function(done) {
+      var stalka = fakeRequestStalka(function(url, callback) {
+        url.should.include("pizza=yummy");
+        callback();
+      });
+
+      stalka.readChanges("http://somehost:1234/somedb", { pizza: 'yummy'}, function(err, changes) {
+        done();
+      });
+    }),
+    it("should query with the correct base url", function(done) {
+      var stalka = fakeRequestStalka(function(url, callback) {
+        url.should.match(/^http:\/\/somehost:1234\/somedb\/_changes?.*/);
+        callback();
+      });
+
+      stalka.readChanges("http://somehost:1234/somedb", null, function(err, changes) {
+        done();
+      });
+    }),
+    it("should return the changes on a successful read", function(done) {
+      var stalka = fakeRequestStalka(function(url, callback) {
+        callback(null, null, {changes: 'yay'});
+      });
+      stalka.readChanges("http://somehost:1234/somedb", null, function(err, changes) {
+        changes.should.eql({changes: 'yay'});
+        done();
+      });
+    }),
+    it("should return the error on an unsuccessful read", function(done) {
+      var stalka = fakeRequestStalka(function(url, callback) {
+        callback("Error reading changes", null, null);
+      });
+      stalka.readChanges("http://somehost:1234/somedb", null, function(err, changes) {
+        err.should.eql("Error reading changes");
+        done();
+      });
+    });
+  }),
+  describe('#start', function() {
+    it("should exit when error reading sequence", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback("Error reading sequence", null);
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        err.should.equal("Error reading sequence")
+        done();
+      });
+    }),
+    it("should exit main loop when stop called", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, {lastSequence: 10});
+      };
+      stalka.readChanges = function(db, options, callback) {
+        callback(null, JSON.stringify({last_seq: 15}));
+      };
+      stalka.updateSequence = function(db, sequenceDoc, callback) {
+        stalka.stop();
+        callback(null, {rev: '123'});
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        should.not.exist(err);
+        done();
+      });
+    }),
+    it("should read changes from the stored local sequence", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, {lastSequence: 11});
+      };
+      stalka.readChanges = function(db, options, callback) {
+        options.since.should.equal(11);
+        stalka.stop();
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        done();
+      });
+    }),
+    it("should read changes from the specified start sequence", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, {lastSequence: 11});
+      };
+      stalka.readChanges = function(db, options, callback) {
+        options.since.should.equal(133);
+        stalka.stop();
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, {since: 133}, function(err) {
+        done();
+      });
+    }),
+    it("should read changes from 0 if no local or specified sequence", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        options.since.should.equal(0);
+        stalka.stop();
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        done();
+      });
+    }),
+    it("should read changes with a default limit of 10", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        options.limit.should.equal(10);
+        stalka.stop();
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        done();
+      });
+    }),
+    it("should read changes with the specified limit", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        options.limit.should.equal(246);
+        stalka.stop();
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, {limit: 246}, function(err) {
+        done();
+      });
+    }),
+    it("should return when read changes fails", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        stalka.stop();
+        callback("Error", null);
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        err.should.equal("Error");
+        done();
+      });
+    }),
+    it("should return when writing changes fails", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        callback(null, JSON.stringify({}));
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback("Error");
+        stalka.stop();
+      }, null, function(err) {
+        err.should.equal("Error");
+        done();
+      });
+    }),
+    it("should return when writing the sequence fails", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, null);
+      };
+      stalka.readChanges = function(db, options, callback) {
+        callback(null, JSON.stringify({}));
+      };
+      stalka.updateSequence = function(db, sequenceDoc, callback) {
+        callback("Error", null);
+        stalka.stop();
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        callback();
+      }, null, function(err) {
+        err.should.equal("Error");
+        done();
+      });
+    }),
+    it("should read and write changes", function(done) {
+      stalka.readSequence = function(db, callback) {
+        callback(null, {lastSequence: 123});
+      };
+      stalka.readChanges = function(db, options, callback) {
+        callback(null, JSON.stringify({last_seq: 200}));
+      };
+      stalka.updateSequence = function(db, sequenceDoc, callback) {
+        sequenceDoc.lastSequence.should.equal(200);
+        stalka.stop();
+        callback(null, {rev: '1234'});
+      };
+      stalka.start("http://randomhost:2422/somedb", function(changes, callback) {
+        var expected = {last_seq: 200};
+        expected.should.eql(changes);
+        callback();
+      }, null, function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
+  });
+});
+
+function fakeRequestStalka(fakeRequestFunction) {
+  return sandbox.require(libpath + '/stalka', {
+    requires: {'nano': fakenano, 'request': fakeRequestFunction}
+  });
+}
